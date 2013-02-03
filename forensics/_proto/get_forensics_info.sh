c17b6c9db048ac595c53c4a61dcdde2dd73dec43
ins.forensics <arrow_13@land.ru>
2013-02-03 11:23:09 +0400

    nothing

--- forensics/_proto/get_forensics_info.sh
+++ forensics/_proto/get_forensics_info.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 DATE=$(date +%F-%H-%M-%S)
-OUTPUT_DIR="./forensics-reports-$DATE"
+O_DIR="./forensics-reports-$DATE"
 
 
 echo Starting forensic analysis...
 
 echo Creating dirs layout...
-mkdir $OUTPUT_DIR
+mkdir $O_DIR
+mkdir $O_DIR/hardware
 
+echo Collecting hardware info...
 
+sudo lshw -html > $O_DIR/hardware/lshw.html
+sudo lshw -json > $O_DIR/hardware/lshw.json
+sudo lshw -xml > $O_DIR/hardware/lshw.xml
 
 echo Checksuming results...
-sha256deep -r $OUTPUT_DIR > "./forensics-reports-$DATE.trh"
+sha256deep -r $O_DIR > "./forensics-reports-$DATE.trh"
 
